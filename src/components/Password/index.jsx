f9c9239c37a58182dbdc0f7f3fba044472eb98ff
doanduc2000 <doanminhducvip2000@gmail.com>
2023-10-20 13:43:34 +0700

    first commit

--- src/components/Password/index.jsx
+++ src/components/Password/index.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './password.module.scss';
 
 const Password = () => {
-  return <div>Password</div>;
+  const [isEdit, setIsEdit] = useState(false);
+  return (
+    <div className={style['password']}>
+      <div className={style['main']}>
+        <div className={style['inputGroup']}>
+          <label htmlFor="password">Mật khẩu mới:</label>
+          <input id="password" type="password" placeholder="Nhập mật khẩu mới" disabled={!isEdit} />
+        </div>
+        <div className={style['inputGroup']}>
+          <label htmlFor="retype">Nhập lại mật khẩu:</label>
+          <input id="retype" type="password" placeholder="Nhập lại mật khẩu" disabled={!isEdit} />
+        </div>
+      </div>
+      <div className={style['control']}>
+        {isEdit ? (
+          <>
+            <button className={style['cancel']} onClick={() => setIsEdit(false)}>
+              Hủy bỏ
+            </button>
+            <button className={style['edit']} onClick={() => setIsEdit(false)}>
+              Lưu thay đổi
+            </button>
+          </>
+        ) : (
+          <button className={style['edit']} onClick={() => setIsEdit(true)}>
+            Đổi mật khẩu
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Password;
